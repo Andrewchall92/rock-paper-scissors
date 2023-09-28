b597dbf6e166722a03713af57009c7abd42c8c49
moxnix <andrewchall92@gmail.com>
2023-09-27 20:27:22 -0700

    We added the function to generate a computer choice, check who wins by compareing userChoice to computerSelection and the function to track the stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // what do we need to track?
 // wins losses and ties
-console.log("meow meow")
 // what do we need to do?
 // gather users input with a prompt
 // play game
@@ -10,48 +9,64 @@ console.log("meow meow")
 var wins = 0;
 var ties = 0;
 var losses = 0;
+var userChoice;
+var computerChoices =["R", "P", "S"];
+var computerSelection;
 
 function playGame(){
-    var userChoice = promptUser();
+    userChoice = promptUser();
+    generateCompChoice();
+    checkWhoWins();
     confrimPlayAgain();
 
-
+}
 
 function promptUser(){
-    var userChoice = prompt("Please enter R, P, or S");
+    userChoice = prompt("Please enter R, P, or S");
     console.log(userChoice);
-    console.log(userChoice.toUpperCase);
+    console.log(userChoice.toUpperCase());
     if(userChoice.toUpperCase() === "R" || userChoice.toUpperCase() === "P" || userChoice.toUpperCase() === "S"){
-        return userChoice
+        return userChoice.toUpperCase();
     } else {
         alert("PLEASE ENTER R P OR S, NO OTHER CHARACTER VALUES WILL BE PERMITTED")
-        promptUser();
+        return promptUser();
     }
 }
 
-} 
 
 function generateCompChoice(){
-    var computerChoices =["R", "P", "S"];
+    var index = Math.floor(Math.random() * computerChoices.length);
+    computerSelection = computerChoices[index];
 }
-var index = Math.floor(Math.random)() * computerChoices.length;
-var computerChoices = 
 
 
-
-
-
-
-
+function checkWhoWins(){
+    if (userChoice === computerSelection){
+        ties++
+        alert("You tied!");
+    } else if ((userChoice === "R" && computerSelection === "S") ||
+              (userChoice === "P" && computerSelection === "R") ||
+              (userChoice === "S" && computerSelection === "P")) {
+        wins++;
+        alert("You win!");
+    }       else {
+        losses++;
+        alert("You lose!");
+    }
+        showStats();
+}
 
 function confrimPlayAgain(){
     var playAgain = confirm("Do you want to play again?")
     if(playAgain){
         playGame();
-    }
-    else{
+    } else{
         alert("Thanks for playing")
     }
+}
+
+function showStats() {
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses);
 }
 
 playGame();
